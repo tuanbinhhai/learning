7dd7c8733ccbc1a7b63f8d64be99eb46eb785030
tuantran <tuanbinhhai@gmail.com>
2018-12-05 15:19:48 +0700

    update code

--- mua-xe-di-vue/src/components/Home/Home.vue
+++ mua-xe-di-vue/src/components/Home/Home.vue
@@ -1,6 +1,156 @@
 <template>
   <div class="home">
     <div class="container">
+      <div class="row row-filter-property">
+        <div class="col-md-5 col-left">
+          <div class="content-up">
+            <h2>TÌM THEO GIÁ</h2>
+            <div class="row price">
+              <div class="col-md-4 col-left">
+                <a href="/search">
+                  <button type="button" class="btn btn-outline-dark btn-price">&lt; 200 triệu</button>
+                </a>
+              </div>
+              <div class="col-md-4 col-mid">
+                <a href="/search">
+                  <button type="button" class="btn btn-outline-dark btn-price">200tr - 400tr</button>
+                </a>
+              </div>
+              <div class="col-md-4 col-right">
+                <a href="/search">
+                  <button type="button" class="btn btn-outline-dark btn-price">400tr - 600tr</button>
+                </a>
+              </div>
+
+              <div class="col-md-4 col-left">
+                <a href="/search">
+                  <button type="button" class="btn btn-outline-dark btn-price">600tr - 1 tỷ</button>
+                </a>
+              </div>
+              <div class="col-md-4 col-mid">
+                <a href="/search">
+                  <button type="button" class="btn btn-outline-dark btn-price">1 tỷ - 1.5 tỷ</button>
+                </a>
+              </div>
+              <div class="col-md-4 col-right">
+                <a href="/search">
+                  <button type="button" class="btn btn-outline-dark btn-price">> 1.5 tỷ</button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-5 col-mid">
+          <div class="content-up">
+            <h2>TÌM THEO HÃNG XE</h2>
+            <div class="row brand">
+              <div class="col-sm-6 col-md-4 col-lg-3 col-left">
+                <a href="/search">
+                  <img src="https://sellcar2017.herokuapp.com/static/img/toyota.75a7766.png" class="brand-hover img-fluid img-brand">
+                </a>
+              </div>
+              <div class="col-sm-6 col-md-4 col-lg-3 col-mid">
+                <a href="/search">
+                  <img src="https://sellcar2017.herokuapp.com/static/img/ford.a481851.png" class="brand-hover img-fluid img-brand">
+                </a>
+              </div>
+              <div class="col-sm-6 col-md-4 col-lg-3 col-mid">
+                <a href="/search">
+                  <img src="https://sellcar2017.herokuapp.com/static/img/lexus.fcd20fc.png" class="brand-hover img-fluid img-brand">
+                </a>
+              </div>
+              <div class="col-sm-6 col-md-4 col-lg-3 col-right">
+                <a href="/search">
+                  <img src="https://sellcar2017.herokuapp.com/static/img/mazda.f2d8578.png" class="brand-hover img-fluid img-brand">
+                </a>
+              </div>
+
+              <div class="col-sm-6 col-md-4 col-lg-3 col-left">
+                <a href="/search">
+                  <img src="https://sellcar2017.herokuapp.com/static/img/chevrolet.5262cca.png" class="brand-hover img-fluid img-brand">
+                </a>
+              </div>
+              <div class="col-sm-6 col-md-4 col-lg-3 col-mid">
+                <a href="/search">
+                  <img src="https://sellcar2017.herokuapp.com/static/img/hyundai.0c1bd24.png" class="brand-hover img-fluid img-brand">
+                </a>
+              </div>
+              <div class="col-sm-6 col-md-4 col-lg-3 col-mid">
+                <a href="/search">
+                  <img src="https://sellcar2017.herokuapp.com/static/img/honda.499b9da.png" class="brand-hover img-fluid img-brand">
+                </a>
+              </div>
+              <div class="col-sm-6 col-md-4 col-lg-3 col-right">
+                <a href="/search">
+                  <img src="https://sellcar2017.herokuapp.com/static/img/mercedes.3ef7962.png" class="brand-hover img-fluid img-brand">
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-2 col-right">
+          <div class="content-up">
+            <h2>NĂM</h2>
+            <div class="row year">
+              <div class="col">
+                <a href="/search">
+                  <button type="button" class="btn btn-outline-dark btn-year">2016</button>
+                </a>
+              </div>
+              <div class="col">
+                <a href="/search">
+                  <button type="button" class="btn btn-outline-dark btn-year">2015</button>
+                </a>
+              </div>
+              <div class="col">
+                <a href="/search">
+                  <button type="button" class="btn btn-outline-dark btn-year">2014</button>
+                </a>
+              </div>
+              <div class="col">
+                <a href="/search">
+                  <button type="button" class="btn btn-outline-dark btn-year">&lt; 2013</button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <a class="col-md-6 col-left">
+          <div class="content-mid">
+            <img src="https://sellcar2017.herokuapp.com/static/img/family.6d322a1.png" class="img-fluid img-full">
+            <h2 class="title-purpose">XE DÙNG CHO GIA ĐÌNH</h2>
+          </div>
+        </a>
+        <a class="col-md-6 col-right">
+          <div class="content-mid">
+            <img src="https://sellcar2017.herokuapp.com/static/img/enterprise.cf0f53c.png" class="img-fluid img-full">
+            <h2 class="title-purpose">XE CHO DOANH NGHIỆP</h2>
+          </div>
+        </a>
+      </div>
+
+      <div class="row">
+        <a class="col-md-4 col-left">
+          <div class="content-mid">
+            <img src="https://sellcar2017.herokuapp.com/static/img/work.26c638a.png" class="img-fluid img-full">
+            <h2 class="title-purpose">XE DÙNG ĐI LẠI</h2>
+          </div>
+        </a>
+        <a class="col-md-4 col-mid">
+          <div class="content-mid">
+            <img src="https://sellcar2017.herokuapp.com/static/img/travel.5b8ee92.png" class="img-fluid img-full">
+            <h2 class="title-purpose">XE DÙNG DU LỊCH</h2>
+          </div>
+        </a>
+        <a class="col-md-4 col-right">
+          <div class="content-mid">
+            <img src="https://sellcar2017.herokuapp.com/static/img/enterprise.cf0f53c.png" class="img-fluid img-full">
+            <h2 class="title-purpose">XE CHẠY DỊCH VỤ</h2>
+          </div>
+        </a>
+      </div>
     </div>
   </div>
 </template>
